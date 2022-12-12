bbc13a8482f10c5706755c7b56af3421ae0fc8f2
Trome29 <108098922+Trome29@users.noreply.github.com>
2022-12-12 11:17:53 +0300

    Initial Commit

---
+++ ColorizedAppSUI/ColorizedAppSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppSUIApp.swift
+//  ColorizedAppSUI
+//
+//  Created by Roman on 12.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
