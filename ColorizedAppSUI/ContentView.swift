78b01c898288fd9c269f8fa9abcfd710028a2ed2
Trome29 <108098922+Trome29@users.noreply.github.com>
2022-12-15 14:47:48 +0300

    minor fixes

--- ColorizedAppSUI/ContentView.swift
+++ ColorizedAppSUI/ContentView.swift
@@ -12,42 +12,37 @@ struct ContentView: View {
     @State private var green = Double.random(in: 0...255).rounded()
     @State private var blue = Double.random(in: 0...255).rounded()
     
-    @FocusState var isInputActive: Bool
+    @FocusState private var focusedField: Field?
     
     var body: some View {
         ZStack {
             Color(.black)
                 .ignoresSafeArea()
                 .onTapGesture {
-                    isInputActive = false
+                    focusedField = nil
                 }
             VStack(spacing: 20) {
-                ColorView(
-                    red: red,
-                    green: green,
-                    blue: blue
-                )
+                ColorView(red: red, green: green, blue: blue)
                 VStack {
                     ColorSliderView(value: $red, color: .red)
+                        .focused($focusedField, equals: .red)
                     ColorSliderView(value: $green, color: .green)
+                        .focused($focusedField, equals: .green)
                     ColorSliderView(value: $blue, color: .blue)
+                        .focused($focusedField, equals: .blue)
                 }
                 .frame(height: 150)
-                .focused($isInputActive)
                 .toolbar {
                         ToolbarItemGroup(placement: .keyboard) {
-                            //Spacer()
-                            Button {
-                                isInputActive = false
-                                
-                            } label: { Image(systemName: "chevron.up") }
-                            Button {
-                                isInputActive = true
-                                
-                            } label: { Image(systemName: "chevron.down") }
+                            Button(action: previousField) {
+                                Image(systemName: "chevron.up")
+                            }
+                            Button(action: nextField) {
+                                Image(systemName: "chevron.down")
+                            }
                             Spacer()
                             Button("Done") {
-                                isInputActive = false
+                                focusedField = nil
                             }
                         }
                     }
@@ -63,5 +58,37 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension ContentView {
+    private enum Field {
+        case red, green, blue
+    }
+    
+    private func nextField() {
+        switch focusedField {
+        case .red:
+            focusedField = .green
+        case .green:
+            focusedField = .blue
+        case .blue:
+            focusedField = .red
+        case .none:
+            focusedField = nil
+        }
+    }
+    
+    private func previousField() {
+        switch focusedField {
+        case .red:
+            focusedField = .blue
+        case .green:
+            focusedField = .red
+        case .blue:
+            focusedField = .green
+        case .none:
+            focusedField = nil
+        }
     }
 }
